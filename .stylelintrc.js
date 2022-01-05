33bebfe6aa839deac1a11ecfb18161b829a04518
zhouleyan <zly_invincible@163.com>
2022-01-05 15:32:51 +0800

    fix(stylelint): remove postcss-less and add scss extend

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'stylelint-config-css-modules',
     'stylelint-config-rational-order',
     'stylelint-no-unsupported-browser-features',
+    'stylelint-config-standard-scss'
   ],
   plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   rules: {
@@ -11,8 +12,7 @@ module.exports = {
     'function-url-quotes': 'always',
     'font-family-no-missing-generic-family-keyword': null,
     'plugin/declaration-block-no-ignored-properties': true,
-    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }]
   },
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts', '**/*.md', '**/*.html', '**/*.ejs', '**/*.hbs'],
-  customSyntax: require('postcss-less')
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts', '**/*.md', '**/*.html', '**/*.ejs', '**/*.hbs']
 };
